50aab2acc1523977b5f71d2b3db98d2bfd599217
imcuttle <moyuyc95@gmail.com>
2019-06-30 23:57:18 +0800

    chore: add myrunnerrc

--- index.js
+++ index.js
@@ -3,4 +3,23 @@
  * @author imcuttle
  */
 
-module.exports = require('./lib/runScriptAdvanced')
+const name = require('./package').name
+const cosmiconfig = require('cosmiconfig')
+const nps = require('path')
+const { runScriptAdvanced } = require('./lib/runScriptAdvanced')
+
+const explorer = cosmiconfig(name)
+
+module.exports = (code, { myrunnerrc = true, ...opts } = {}) => {
+  const searchedFor = explorer.searchSync()
+
+  if (searchedFor.config) {
+    opts = {
+      rootDir: nps.dirname(searchedFor.filepath),
+      ...searchedFor.config,
+      ...opts
+    }
+  }
+
+  return runScriptAdvanced(code, opts)
+}
