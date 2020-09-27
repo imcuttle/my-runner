d646944d801de2ce1a415c6743eb7749d7844269
余聪 <yucong06@meituan.com>
2020-09-27 16:25:18 +0800

    chore: passing fs

--- __tests__/resolvePath.test.js
+++ __tests__/resolvePath.test.js
@@ -4,6 +4,9 @@
  * @date 2019/6/26
  * @description
  */
+const fs = require('fs')
+const MemoryFs = require('memory-fs')
+const { dirname } = require('path')
 const { fixture } = require('./helper')
 const resolvePath = require('../lib/utils/resolvePath')
 
@@ -11,6 +14,7 @@ describe('resolvePath', function() {
   it('should resolvePath spec', () => {
     expect(fixture('./resolvePath/spec/index.js')).toBe(
       resolvePath('./spec', {
+        fs,
         basedir: fixture('./resolvePath')
       })
     )
@@ -19,6 +23,7 @@ describe('resolvePath', function() {
   it('should resolvePath extensions', () => {
     expect(fixture('./resolvePath/spec/index.jsx')).toBe(
       resolvePath('./spec', {
+        fs,
         moduleFileExtensions: ['.jsx', '.js'],
         basedir: fixture('./resolvePath')
       })
@@ -28,6 +33,7 @@ describe('resolvePath', function() {
   it('should resolvePath moduleDirectories', () => {
     expect(fixture('./resolvePath/modules/foo.js')).toBe(
       resolvePath('foo', {
+        fs,
         moduleDirectories: ['modules'],
         basedir: fixture('./resolvePath')
       })
@@ -37,6 +43,7 @@ describe('resolvePath', function() {
   it('should resolvePath moduleDirectories array', () => {
     expect(fixture('./resolvePath/high-modules/foo.js')).toBe(
       resolvePath('foo', {
+        fs,
         moduleDirectories: ['high-modules', 'modules'],
         basedir: fixture('./resolvePath')
       })
@@ -44,6 +51,7 @@ describe('resolvePath', function() {
 
     expect(fixture('./resolvePath/modules/bar.js')).toBe(
       resolvePath('bar', {
+        fs,
         moduleDirectories: ['high-modules', 'modules'],
         basedir: fixture('./resolvePath')
       })
@@ -53,6 +61,7 @@ describe('resolvePath', function() {
   it('should resolvePath browser', () => {
     expect(fixture('./resolvePath/browser/main.js')).toBe(
       resolvePath('.', {
+        fs,
         basedir: fixture('./resolvePath/browser')
       })
     )
@@ -60,6 +69,7 @@ describe('resolvePath', function() {
     expect(fixture('./resolvePath/browser/browser.js')).toBe(
       resolvePath('./', {
         browser: true,
+        fs,
         basedir: fixture('./resolvePath/browser')
       })
     )
@@ -68,9 +78,57 @@ describe('resolvePath', function() {
   it('should resolvePath - modulePaths', () => {
     expect(fixture('./resolvePath/paths/modules/foo.js')).toBe(
       resolvePath('foo', {
+        fs,
         modulePaths: [fixture('./resolvePath/paths/modules')],
         basedir: fixture('./resolvePath/paths')
       })
     )
+  })
+
+  describe('memoryFs', function() {
+    let memoryFs
+    function writeFiles(files) {
+      for (const [name, data] of Object.entries(files)) {
+        memoryFs.mkdirpSync(dirname(name))
+        memoryFs.writeFileSync(name, String(data))
+      }
+    }
+    beforeEach(() => {
+      memoryFs = new MemoryFs()
+    })
+
+    it('should resolvePath - normal', function() {
+      writeFiles({
+        '/index.js': ' '
+      })
+
+      expect('/index.js').toBe(
+        resolvePath('./index.js', {
+          basedir: '/',
+          fs: memoryFs
+        })
+      )
+    })
+
+    it('should resolvePath - browser', function() {
+      writeFiles({
+        '/node_modules/foo/package.json': JSON.stringify({
+          name: 'foo',
+          browser: {
+            './index.js': 'index.browser.js'
+          }
+        }),
+        '/node_modules/foo/index.js': ' ',
+        '/node_modules/foo/index.browser.js': ' '
+      })
+
+      expect('/node_modules/foo/index.browser.js').toBe(
+        resolvePath('foo', {
+          browser: true,
+          basedir: '/',
+          fs: memoryFs
+        })
+      )
+    })
   })
 })
